da02a9d987563ca3bfdae6cdd99ccd776633060c
suryashankarungarala <122678702+suryashankarungarala@users.noreply.github.com>
2023-05-11 06:03:12 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,18 +17,6 @@ resource "aws_instance" "one" {
   }
 }
 
-resource "aws_instance" "two" {
-  ami             = "ami-03c7d01cf4dedc891"
-  instance_type   = "t2.micro"
-  key_name        = "surya"
-  vpc_security_group_ids = [aws_security_group.three.id]
-  availability_zone = "us-east-1a"
-    tags = {
-    Name = "web-server-2"
-  }
-}
-
-
 resource "aws_security_group" "three" {
   name = "elb-sg"
   ingress {
